67f973bd794ae4a17ed7e1354600b26f5b2fdef7
Baysaaaa <bayas.chuluun@gmail.com>
2021-11-08 23:17:19 +0100

    in <backend> folder, created .env file with environment variables (DB URI, DB Name, PORT number)

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // means that the server app can accept/read JSON in the body of a request (e.g. GET, POST, etc)
 
-app.use("/api/v1/restaurants", restaurants); // where "/api/v1/restaurants" is the inital route i.e. URL
-app.use("*", (req, res) => res.status(404).json({ error: "not found" })); // if user goes to any other route (i.e. route is "*"), returns a 404 page that says "not found"
+app.use("/api/v1/restaurants", restaurants); // where "/api/v1/restaurants" is the inital route i.e. initial URL
+app.use("*", (req, res) => res.status(404).json({ error: "not found" })); // if user goes to any other route (captured by "*"), returns a 404 page that says "not found"
 
 export default app;

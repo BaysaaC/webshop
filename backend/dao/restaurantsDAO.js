44740737114a4b8e0cfc6164583f094ddf3a79f1
Baysaaaa <bayas.chuluun@gmail.com>
2021-11-09 14:50:58 +0100

    added semi-colons to lines of code covered so far in the controller file and in the DAO file

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -21,39 +21,38 @@ export default class RestaurantsDAO {
         page = 0,
         restaurantsPerPage = 20,
     } = {}) {
-        let query
+        let query;
         if (filters) {
             if ("name" in filters) {
-                query = { $text: { $search: filters["name"] } }
+                query = { $text: { $search: filters["name"] } };
             } else if ("cuisine" in filters) {
-                query = { "cuisine": { $eq: filters["cuisine"] } }
+                query = { "cuisine": { $eq: filters["cuisine"] } };
             } else if ("zipcode" in filters) {
-                query = { "address.zipcode": { $eq: filters["zipcode"] } }
+                query = { "address.zipcode": { $eq: filters["zipcode"] } };
             }
         }
 
-        let cursor
-
+        let cursor;
         try {
             cursor = await restaurants
-                .find(query)
+                .find(query);
         } catch (e) {
-            console.error(`Unable to issue find command, ${e}`)
-            return { restaurantsList: [], totalNumRestaurants: 0 }
+            console.error(`Unable to issue find command, ${e}`);
+            return { restaurantsList: [], totalNumRestaurants: 0 };
         }
 
-        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
+        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page);
 
         try {
-            const restaurantsList = await displayCursor.toArray()
-            const totalNumRestaurants = await restaurants.countDocuments(query)
+            const restaurantsList = await displayCursor.toArray();
+            const totalNumRestaurants = await restaurants.countDocuments(query);
 
-            return { restaurantsList, totalNumRestaurants }
+            return { restaurantsList, totalNumRestaurants };
         } catch (e) {
             console.error(
                 `Unable to convert cursor to array or problem counting documents, ${e}`,
-            )
-            return { restaurantsList: [], totalNumRestaurants: 0 }
+            );
+            return { restaurantsList: [], totalNumRestaurants: 0 };
         }
     }
     static async getRestaurantByID(id) {
